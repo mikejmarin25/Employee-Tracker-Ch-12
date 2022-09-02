bb6690f1be5b9a9d5def31b658423f88b7e1b4ef
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-09-01 21:08:34 -0400

    reformatted SQL files

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,38 +1,46 @@
-USE employeesDB;
+INSERT INTO department (name)
+VALUES ("Executive"),
+       ("Engineering"),
+       ("Sales"),
+       ("Marketing"),
+       ("Finance"),
+       ("Legal"),
+       ("HR");
 
-INSERT INTO department (name)
-VALUES ("Sales");
-INSERT INTO department (name)
-VALUES ("Engineering");
-INSERT INTO department (name)
-VALUES ("Testing");
-INSERT INTO department (name)
-VALUES ("Administration");
+
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Representative", 100000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Manager", 150000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("QA Tester", 125000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Administrative Manager", 250000, 4);
+VALUES ("President", 500000, 1),
+       ("CEO", 400000, 1),
+       ("CFO", 250000, 1),
+       ("Lead Software Engineer", 150000, 2),
+       ("Software Engineer", 130000, 2),
+       ("QA Tester", 10000, 2),
+       ("Lead Sales", 120000, 3),
+       ("Associate Sales", 80000, 3),
+       ("Marketing Manager", 100000, 4),
+       ("Marketing Associate", 60000, 4),
+       ("Account Manager", 160000, 5),
+       ("Accountant", 125000, 5),
+       ("Legal Team Lead", 250000, 6),
+       ("Lawyer", 200000, 6),
+       ("HR Manager", 90000, 7),
+       ("HR Associate", 60000, 7);
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Eddard", "Stark", 1, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sansa", "Stark", 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tyrion", "Lannister", 3, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Brienne", "Tarth", 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tywin", "Lannister", 5, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Aria", "Stark", 2, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Daenerys", "Targaryen", 4, 7);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Snow", 1, 2);
+VALUES ("Eddard", "Stark", 1, 14),
+       ("Sansa", "Stark", 2, 1),
+       ("Tony", "Stark", 3, 14),
+       ("Tyrion", "Lannister", 4, 3),
+       ("Brienne", "Tarth", 5, 14),
+       ("Tywin", "Lannister", 6, 5),
+       ("Aria", "Stark", 7, 14),
+       ("Peter", "Balish", 8, 7),
+       ("Daenerys", "Targaryen", 9, 14),
+       ("John", "Snow", 10, 9),
+       ("Jamie", "Lannister", 11, 14),
+       ("Bran", "Stark", 12, 11),
+       ("Roose", "Bolton", 13, null),
+       ("Catelyn", "Stark", 14, 13),
+       ("Theon", "Greyjoy", 15, 13);
